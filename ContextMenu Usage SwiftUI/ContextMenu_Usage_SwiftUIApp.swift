c752096357383f50da3b607714b611263cd3382b
Omer Faruk Kilicaslan <omerrr_2001@hotmail.com>
2022-05-12 12:44:59 +0300

    Initial Commit

---
+++ ContextMenu Usage SwiftUI/ContextMenu_Usage_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContextMenu_Usage_SwiftUIApp.swift
+//  ContextMenu Usage SwiftUI
+//
+//  Created by Ömer Faruk Kılıçaslan on 12.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContextMenu_Usage_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
